609d523ec617f4804337a305d790ccbc339aa842
Maxim Movchan <maksmovchan@icloud.com>
2022-01-06 22:05:02 +0300

    'new'

--- lib/data/model/data/url_list_model.dart
+++ lib/data/model/data/url_list_model.dart
@@ -1,25 +1,24 @@
+import 'dart:convert';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class UrlList {
-
   List<String> urlList = List.empty(growable: true);
+  List<String> dateList = List.empty(growable: true);
 
   clearList() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.clear();
   }
 
-  saveList(List<String> gotList) async {
-    List list = gotList;
-    list = list.map((item) => json.encode(item)).toList();
+  saveLists() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    prefs.setStringList('history_url', list as List<String>);
-    print('saved in func');
+    prefs.setStringList('history_urls', urlList);
+    prefs.setStringList('history_dates', dateList);
   }
 
-  Future<List<String>> getList() async {
+  Future<void> getUrlList() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    var gotList = prefs.getStringList('history_url');
+    var gotList = prefs.getStringList('history_urls');
     if (gotList != null) {
       List<String> dataSet = List.empty(growable: true);
       for (int i = 0; i < gotList.length; i++) {
@@ -27,24 +26,35 @@ class UrlList {
         item = gotList[i].replaceAll('"', '');
         dataSet.add(item);
       }
-      return dataSet;
+      urlList=dataSet;
     } else {
-      return [];
+      urlList=[];
+    }
+  }
+  Future<void> getDatesList() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var gotList = prefs.getStringList('history_dates');
+    if (gotList != null) {
+      List<String> dataSet = List.empty(growable: true);
+      for (int i = 0; i < gotList.length; i++) {
+        String item;
+        item = gotList[i].replaceAll('"', '');
+        dataSet.add(item);
+      }
+      dateList=dataSet;
+    } else {
+      dateList=[];
     }
   }
 
-  addList(String data) async {
-    List<String> list = await getList();
-    list.add(data);
-    for (int i = 0; i < list.length; i++) {
-      print(list[i]);
-    }
-    await saveList(list);
+  addToUrlList(String data) async {
+    print('add to url');
+    print(data);
+    urlList.add(data);
+    await saveLists();
   }
-
-  removeLast() async {
-    List<String> list = await getList();
-    list.removeLast();
-    await saveList(list);
+  addToDatesList(String data) async {
+    dateList.add(data);
+    await saveLists();
   }
 }
